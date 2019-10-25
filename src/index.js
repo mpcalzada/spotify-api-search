eee62b090ab62fc425c8cf361eec451f3c47c105
Marco Calzada <mcalzada@kssoluciones.com.mx>
2019-10-25 17:57:59 -0500

    Adding new functions for searching artist

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ const spotify = require("./plugins/spotify")
 spotify.getTrack('Otro Trago')
     .then(obj => console.log(obj))
     .catch(err => console.log(err))
+
+
+spotify.getArtist('Otro Trago')
+    .then(obj => console.log(obj))
+    .catch(err => console.log(err))
--- src/plugins/spotify.js
+++ src/plugins/spotify.js
@@ -1,24 +1,43 @@
 const Spotify = require('node-spotify-api')
+const { spotifySearchURL } = require('../plugins/secrets')
+import axios from 'axios';
 
 const spotify = new Spotify({
-    id: process.env.SPOTIFY_CLIENT_ID,
-    secret: process.env.SPOTIFY_CLIENT_SECRET
+  id: process.env.SPOTIFY_CLIENT_ID,
+  secret: process.env.SPOTIFY_CLIENT_SECRET
 })
 
+
+
 const getTrack = async (query = '', limit = 1) => {
-    const response = await spotify.search({ type: 'track', query, limit })
+  const response = await spotify.search({ type: 'track', query, limit })
 
-    const obj = response.tracks.items.map(item => {
-      let artists;
-      item.artists.map(artist => artists = !artists ? artists = artist.name : artists + `, ${artist.name}`)
-      return {
-        name: item.name,
-        preview: item.preview_url,
-        artist: artists
-      };
+  const obj = response.tracks.items.map(item => {
+    let artists;
+    item.artists.map(artist => artists = !artists ? artists = artist.name : artists + `, ${artist.name}`)
+    return {
+      name: item.name,
+      preview: item.preview_url,
+      artist: artists
+    };
+  })
+
+  return obj;
+}
+
+const getArtist = (query = '', authToken = '') => {
+  let artists;
+  axios.get(`${spotifySearchURL}${query}&type=artist&access_token=${authToken}`)
+    .then(response => {
+      artists = response.data.artists;
+      this.setState({ artists, error: '' });
     })
-
-    return obj;
-  }
+    .catch(error => {
+      this.setState({
+        error: 'Sorry your search didn\'t return any results...'
+      })
+    })
+}
 
 module.exports.getTrack = getTrack;
+module.exports.getArtist = getArtist;
